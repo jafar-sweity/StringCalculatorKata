07a2375d79296401c34d41dfbd84f6e9e8686830
jafar fuad <jata1518@gmail.com>
2025-04-18 16:51:47 +0300

    Implement the simplest Add function

--- StringCalculatorKata/Program.cs
+++ StringCalculatorKata/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using StringCalculatorKata.Services;
+
 Console.WriteLine("Hello, World!");
+
+var calculator = new StringCalculator();
+// Test case 1: empty string
+var result = calculator.Add("");
+Console.WriteLine(result); // Expected output: 0
+// Test case 2: single number
+ var result1 = calculator.Add("1");
+Console.WriteLine(result1); // Expected output: 1
+// Test case 3: two numbers
+var result2 = calculator.Add("1,2");
+Console.WriteLine(result2); // Expected output: 3
--- StringCalculatorKata/Services/StringCalculator.cs
+++ StringCalculatorKata/Services/StringCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace StringCalculatorKata.Services
 {
-    class StringCalculator
+    public class StringCalculator
     {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            var split = numbers.Split(',');
+
+            if (split.Length == 1)
+                return int.Parse(split[0]);
+
+            return int.Parse(split[0])+ int.Parse(split[1]);
+        }
     }
 }
